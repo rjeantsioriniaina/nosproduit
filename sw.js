07bed1c3b68ade8e6f9b8a17fbd2a3289cd44215
meilleursProduits <rjeantsioriniaina@gmail.com>
2024-09-09 07:54:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,14 @@
-// Nom du cache
-const CACHE_NAME = 'v1.0';
-
-// URL des fichiers à mettre en cache
-const CACHE_URLS = [
-    '/',
-    'css/style.css',
-    'images/image-produit1.webp',
-    'images/image-produit2.webp',
-    'images/image-produit3.webp',
-    'images/temoignage1.webp',
-    'images/temoignage2.webp',
-];
-
-// Événement d'installation du service worker
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(CACHE_URLS);
-        })
-    );
+    console.log('Service Worker installé.');
+    // Précharge des ressources ici si nécessaire
 });
 
-// Événement de gestion des requêtes
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activé.');
+    // Supprime les anciens caches ici si nécessaire
+});
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    console.log('Fetching:', event.request.url);
+    // Gérer les requêtes ici, par exemple avec un cache
 });
