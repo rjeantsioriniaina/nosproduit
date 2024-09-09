f5cf35f141497ad86162ccdff77f90b7e577b130
meilleursProduits <rjeantsioriniaina@gmail.com>
2024-09-09 08:02:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
 self.addEventListener('install', (event) => {
     console.log('Service Worker installé.');
-    // Précharge des ressources ici si nécessaire
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker activé.');
-    // Supprime les anciens caches ici si nécessaire
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
-    // Gérer les requêtes ici, par exemple avec un cache
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
